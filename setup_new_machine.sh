5696b63c41ab2ebda0a75e89fc3ed8cde9eca71c
Miro Babić <mbabic131@gmail.com>
2017-10-19 13:26:57 +0200

    Create setup_new_machine.sh

---
+++ setup_new_machine.sh
@@ -0,0 +1,38 @@
+
+#!/usr/bin/env bash
+set -e
+
+# For Oracle/ Sun JDK 8.
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install \
+    curl \
+    language-pack-en \
+    git \
+    oracle-java8-installer \
+    python-pip \
+    zip \
+    unzip \
+    screen \
+    htop \
+    vnstat \
+    tmux \
+    grc \
+    ncdu \
+    nano \
+    icdiff \
+    weechat \
+    autojump \
+    mosh \
+    silversearcher-ag \
+    zsh
+
+sudo pip install -U mycli
+
+echo "Cleaning Up" &&
+sudo apt-get -f install &&
+sudo apt-get autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y clean
